465ad56124cf2940c54105f2195fbf22fa103c05
luoyue <15313266013@163.com>
2022-07-13 21:38:59 +0800

    20220713socket学习

---
+++ python_socket/10_TCP客户端开发.py
@@ -0,0 +1,16 @@
+import socket
+
+if __name__ == '__main__':
+
+    #1.创建客户端socket    AF_INET表示IPv4  SOCK_STREAM表示TCP协议
+    tcp_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    #2.与服务器建立连接
+    tcp_client.connect(("",8080))
+    #3.发送数据
+    tcp_client.send('nihao'.encode(encoding='utf-8'))
+    #4.接收数据
+    recv_data = tcp_client.recv(1024)
+
+    print(recv_data.decode())
+    #5.关闭socket
+    tcp_client.close()
